b85225b2f056eae31e100970a47405be67056bb0
ruvl <ru.vlad.13@gmail.com>
2024-02-18 16:34:14 +0300

    Change rootLogger format style

--- scripts/logger.py
+++ scripts/logger.py
@@ -3,8 +3,14 @@ import logging
 rootLogger = logging.getLogger()
 rootLogger.setLevel(logging.INFO)
 
+formatter = logging.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y')
+
 fileHandler = logging.FileHandler('scripts.log', encoding='utf-8')
+fileHandler.setFormatter(formatter)
 rootLogger.addHandler(fileHandler)
 
 consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(formatter)
 rootLogger.addHandler(consoleHandler)
+
+logging.basicConfig()
