23c1acc8be7d81a31e5856f647bd234f7e8d574f
ruvl <ru.vlad.13@gmail.com>
2024-02-23 01:34:20 +0300

    Отсутствует if visit name is None

--- core/text/shared.py
+++ core/text/shared.py
@@ -68,7 +68,7 @@ async def face_info_text(
 
     # All names with dates and locations
     name_and_date_str = '\n'.join(
-        f"{escape_markdown_v2(visit.name)} "
+        f"{escape_markdown_v2(visit.name) if visit.name is not None else 'Отсутствует'} "
         f"\({visit.date:%H:%M %d\.%m\.%Y}\) "
         f"\- {escape_markdown_v2(visit.location.address) if visit.location is not None else 'отсутствует'}"
         for visit in visits
