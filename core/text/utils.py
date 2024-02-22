5c1782203492f98294589e5b26f4b57f48c1fe32
ruvl <ru.vlad.13@gmail.com>
2024-02-23 01:31:49 +0300

    escape None fix

--- core/text/utils.py
+++ core/text/utils.py
@@ -1,5 +1,10 @@
 import re
 
 
-def escape_markdown_v2(text: str) -> str:
+def escape_markdown_v2(text: str | None) -> str:
+    """ Escape str or return None if None is provided """
+
+    if text is None:
+        return None
+
     return re.sub(r'([_*\[\]()~`>#+\-=|{}.!])', r'\\\1', text)
