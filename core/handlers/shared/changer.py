6a979ef884a7a908a49f625cbed521c6faa08d98
ruvl <ru.vlad.13@gmail.com>
2024-02-24 13:13:14 +0300

    Fix delete_client -> no 2

--- core/handlers/shared/changer.py
+++ core/handlers/shared/changer.py
@@ -78,9 +78,7 @@ async def delete_client(callback: types.CallbackQuery, state: FSMContext):
             await return2start_menu(callback, state)
         case 'no':
             await state.set_state(SharedMenu.SHOW_FACE_INFO)
-            await state.set_state(SharedMenu.SHOW_FACE_INFO)
-
-            await add_visit_back(callback, state)
+            await callback.answer()
 
             keyboard = add_visit_kb(user_id=callback.from_user.id)
             await show_client(callback.message, state, reply_markup=keyboard)
