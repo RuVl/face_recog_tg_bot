89eafee2d75e65533d658bdbb1fc4ce7b2a09c53
ruvl <ru.vlad.13@gmail.com>
2024-02-25 17:07:28 +0300

    Fix multiply middlewares

--- core/handlers/main.py
+++ core/handlers/main.py
@@ -10,9 +10,7 @@ def register_all_handlers(dp: Dispatcher) -> None:
     # Keep order
     main_router.include_routers(admin_router, moderator_router, admin_moderator_router, anyone_router)
 
-    main_router.callback_query.outer_middleware(
-        ThrottlingMiddleware(dp.storage),
-        DropEmptyButtonMiddleware()
-    )
+    main_router.callback_query.outer_middleware(ThrottlingMiddleware(dp.storage))
+    main_router.callback_query.outer_middleware(DropEmptyButtonMiddleware())
 
     dp.include_router(main_router)
