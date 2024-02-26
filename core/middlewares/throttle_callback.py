8a9188e5ed6eafa05925a27be52570a6500fa136
ruvl <ru.vlad.13@gmail.com>
2024-02-26 20:45:55 +0300

    Fix ThrottlingMiddleware. Add timeout for 5 seconds if throttling was happened or handler was not responded

--- core/middlewares/throttle_callback.py
+++ core/middlewares/throttle_callback.py
@@ -1,17 +1,20 @@
 import logging
+from datetime import datetime, timedelta
 from typing import Awaitable, Any, Callable
 
 from aiogram import BaseMiddleware
 from aiogram import types
-from aiogram.dispatcher.event.bases import SkipHandler
+from aiogram.dispatcher.event.bases import CancelHandler
 from aiogram.fsm.storage.base import StorageKey
 
 from core.misc.utils import get_storage
 
 
 class ThrottlingMiddleware(BaseMiddleware):
-    def __init__(self, prefix: str = 'antiflood'):
+    def __init__(self, prefix: str = 'antiflood', timeout=timedelta(seconds=5)):
         self.storage = get_storage(key_builder_prefix=prefix)
+        self.timeout = timeout
+        
         super(ThrottlingMiddleware, self).__init__()
 
     async def __call__(
@@ -31,18 +34,24 @@ class ThrottlingMiddleware(BaseMiddleware):
             user_id=event.message.from_user.id
         )
 
-        was_pressed: bool = await self.storage.get_data(key=key)
+        now_ = datetime.now()
+        antiflood_data: dict = await self.storage.get_data(key=key) or {}
 
-        if was_pressed:
+        antiflood_data.setdefault('response_received', False)
+        antiflood_data.setdefault('callback_time', now_)
+
+        # Cancel handler
+        if not antiflood_data.get('response_received') and now_ - antiflood_data['callback_time'] < self.timeout:
             await event.answer()
-            raise SkipHandler()
+            raise CancelHandler()
 
-        await self.storage.set_data(key=key, data=True)
+        await self.storage.set_data(key=key, data=antiflood_data)
 
         try:
             result = await handler(event, data)
         finally:
-            await self.storage.set_data(key=key, data=False)
+            antiflood_data['response_received'] = True
+            await self.storage.set_data(key=key, data=antiflood_data)
 
         return result
 
