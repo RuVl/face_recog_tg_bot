b85e3149459613a87900f7e31012a87cee2b10e6
ruvl <ru.vlad.13@gmail.com>
2024-03-10 16:38:17 +0300

    Migrate to a newer version of deepface

--- core/face_recognition/main.py
+++ core/face_recognition/main.py
@@ -1,15 +1,15 @@
-from deepface.commons import distance as dst
+from deepface.modules import verification as dst
 
 from core.config import MODEL, DISTANCE_METRIC
 
 
 def get_distance(distance_metric, embedding1, embedding2) -> int:
     if distance_metric == "cosine":
-        return dst.findCosineDistance(embedding1, embedding2)
+        return dst.find_cosine_distance(embedding1, embedding2)
     elif distance_metric == "euclidean":
-        return dst.findEuclideanDistance(embedding1, embedding2)
+        return dst.find_euclidean_distance(embedding1, embedding2)
     elif distance_metric == "euclidean_l2":
-        return dst.findEuclideanDistance(
+        return dst.find_euclidean_distance(
             dst.l2_normalize(embedding1), dst.l2_normalize(embedding2)
         )
     else:
@@ -21,7 +21,7 @@ def compare_faces(known_faces: list[dict], face2compare: dict):
         :param known_faces: list of known face encodings
         :param face2compare: face encoding to find similar faces
     """
-    threshold = dst.findThreshold(MODEL, DISTANCE_METRIC)
+    threshold = dst.find_threshold(MODEL, DISTANCE_METRIC)
 
     result = []
     for face in known_faces:
