a40c49d62051aade6de06ee68e6cc75d24edd99b
ruvl <ru.vlad.13@gmail.com>
2024-03-10 19:04:39 +0300

    Get meta before returning cloud_obj

--- core/database/methods/video/utils.py
+++ core/database/methods/video/utils.py
@@ -11,23 +11,21 @@ async def _send_video2cloud(video_id: str | int, path: str | Path):
         path = str(path)
 
     logging.info(f'Uploading video for {video_id} record...')
-    data, new_path = await upload_file(path, MEDIA_DIR)
+    cloud_obj, new_path = await upload_file(path, MEDIA_DIR)
 
-    if not data:
+    if not cloud_obj:
         logging.warning('Cannot upload video!')
         return
 
+    logging.info(f'PATH: {cloud_obj.path} HREF: {cloud_obj.href}')
     cloud_data = {
-        'path': data.path,
-        'href': data.href,
-        'file': data.file,
-        'type': data.type,
-        'public_url': data.public_url,
-        'public_key': data.public_key
+        'path': cloud_obj.path,
+        'href': cloud_obj.href,
+        'public_url': cloud_obj.public_url
     }
 
     logging.info(f"Cutting video's ({video_id}) public_url...")
-    url = await cut_url(data.public_url)
+    url = await cut_url(cloud_obj.public_url)
 
     logging.info(f'Updating {video_id} record...')
     await update_video_fields(video_id, path=new_path, cloud_data=cloud_data, url=url)
